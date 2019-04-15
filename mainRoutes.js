6862a0c8a72e93d12afd136ee18dae6d9d965c6b
Sachin Laloo <sachinlaloo@gmail.com>
2019-04-15 22:59:54 +0200

    edited message

--- mainRoutes.js
+++ mainRoutes.js
@@ -2,7 +2,7 @@ let path = require('path')
 let express = require('express')
 let mainRouter = express.Router()
 mainRouter.get('/', function (req, res) {
-  res.send('Hello World, I\'m Node.js')
+  res.send('Hello World, I\'m Node.js, ChangeSSSSSS')
 })
 mainRouter.get('/about', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'about.html'))
