92f266ee61b0dbb87f53585e598f1b1cc6fe645d
Sachin Laloo <sachinlaloo@gmail.com>
2019-04-17 18:14:52 +0200

    Set up for file management

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', mainRouter)
 app.use('/class', classRouter)
 
+app.use('/cdn', express.static('public')) /* this will mount your public
+directory to '/cdn'. i.e. your scripts folder will be at /cdn/scripts */
+
 let port = process.env.PORT || 3000
 app.listen(port)
 console.log('Express server running on port', port)
